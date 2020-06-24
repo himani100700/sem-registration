93421ccf35001ee751d7a103b9454e5f5c50f76e
himani chauhan <himani100700@gmail.com>
2020-06-24 20:19:18 +0530

    std login done

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CreatestdService } from '../createstd.service';
 import { Router } from '@angular/router';
+import { FormGroup,FormControl } from '@angular/forms'
+
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  loginStdData :Object = {
-    full_name: "" ,
-    student_no: ""
-  };
+  loginStdData= new FormGroup({
+    full_name: new FormControl(''),
+    student_no: new FormControl('') 
+  }) ;
 
-  constructor(private service: CreatestdService ) { }
+  constructor(private service: CreatestdService, private _router:Router) {
+    
+  } 
   
   loginStd(){
     
-    this.service.loginStd(this.loginStdData).subscribe(
-      res => console.log(res),
+    this.service.loginStd(this.loginStdData.value).subscribe(
+      res =>{ console.log(res)
+        this._router.navigate(["proceed"])
+      },
       err => console.log(err),
-      // this._router.navigate(["/proceed"])
+       
     )
+
+    
   }
 
   ngOnInit(): void {
