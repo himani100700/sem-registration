8d67eceef0c3e1a9bce96137ce4a9ba3fe5de467
himani chauhan <himani100700@gmail.com>
2020-06-29 18:43:40 +0530

    chnge in proceed

--- src/app/proceed/proceed.component.ts
+++ src/app/proceed/proceed.component.ts
@@ -12,14 +12,15 @@ export class ProceedComponent implements OnInit {
   constructor(private service: CreatestdService, private _router:Router) { }
    checkdues()
    {
-     this.service.stdDue().subscribe(
-       data=>{console.warn(data)
-        this._router.navigate(["form"]);},
-      err =>{ 
-        console.warn(err)
-        alert("Please clear out your dues before you proceed")
-      })
-      console.warn(this.service.logindata)
+    this._router.navigate(["form"]);
+    //  this.service.stdDue().subscribe(
+    //    data=>{console.warn(data)
+    //     this._router.navigate(["form"]);},
+    //   err =>{ 
+    //     console.warn(err)
+    //     alert("Please clear out your dues before you proceed")
+    //   })
+    //   console.warn(this.service.logindata)
    }
   ngOnInit(): void {
   }
