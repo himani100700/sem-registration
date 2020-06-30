e161c47ce742ecfe30c6491e3c5dbd84d0f65ecd
himani chauhan <himani100700@gmail.com>
2020-07-01 00:30:59 +0530

    faculy login toastr

--- src/app/faculty/faculty.component.ts
+++ src/app/faculty/faculty.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CreatestdService } from '../createstd.service';
 import { Router } from '@angular/router';
 import { FormGroup,FormControl } from '@angular/forms'
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-faculty',
@@ -9,22 +10,39 @@ import { FormGroup,FormControl } from '@angular/forms'
   styleUrls: ['./faculty.component.css']
 })
 export class FacultyComponent implements OnInit {
-  alert:boolean=false
+  isLoading = false;
   loginFacultyData= new FormGroup({
     username: new FormControl(''),
     password: new FormControl('') 
   }) ;
 
-  constructor(private service: CreatestdService, private _router:Router) { }
+  constructor(private service: CreatestdService, private _router:Router,private toastr: ToastrService,) { }
   errormsg:string;
   loginFaculty(){
+    if (this.loginFacultyData.value.username=="" || this.loginFacultyData.value.password=="" ) {
+      this.toastr.warning("Please fill all fields first!", "Error",{
+       closeButton:true,
+       "positionClass": "toast-top-center",
+     });
+     return;
+     }
+     this.isLoading = true;
     this.service.facultyLogin(this.loginFacultyData.value).subscribe(
       res =>{ 
-        localStorage.setItem('token',res.token)
-        this._router.navigate(['facultypage/dashboard'])
+        this.toastr.success("Login Successfull!","",{
+          closeButton:true,
+          "positionClass": "toast-top-center",
+        })
+        localStorage.setItem('token',res.token);
+        this.isLoading = false;
+        this._router.navigate(['facultypage/dashboard']);
       },
       err =>{ console.log(err)
-        this.alert=true
+        this.isLoading = false;
+        this.toastr.error("Invalid Login Credentials!","",{
+          closeButton:true,
+          "positionClass": "toast-top-center",
+        })
       } 
     )
   }
